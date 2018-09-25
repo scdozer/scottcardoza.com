b2f51c1537b6b428e15928b644477baea2b892e1
scdozer <scottccardoza@gmail.com>
2018-09-25 15:08:14 -0600

    slight changes

--- src/components/work/work.js
+++ src/components/work/work.js
@@ -8,24 +8,21 @@ export default class Work extends Component {
         <h2>EXPERIENCE</h2>
         <hr></hr>
         <h3>CONSUMER FOCUS MARKETING</h3>
-        <p>Web Developer. Feb. 2016-Present. <a href="http://consumerfocusmarketing.com/" target="_blank">Visit Site.</a></p>
+        <p>Web Developer. Feb. 2016-Present. <a href="https://consumerfocusmarketing.com/" target="_blank">Visit Site.</a></p>
 
-        <ul>
+        {/* <ul>
           <li><a href="https://nhsaveswithoil.com/" target="_blank">NH Saves With Oil</a> - Custom Wordpress.</li>
           <li><a href="http://easternenergyexpo.com/" target="_blank">Eastern Energy Expo</a> - Custom Wordpress. </li>
           <li><a href="http://mpproperties.net/" target="_blank">Murray Custom Properties</a> - Custom Wordpress.</li>
           <li><a href="http://snowshomeandgarden.com/" target="_blank">Snows Home and Garden</a> - Custom Wordpress. E-Commerce.</li>
           <li><a href="http://papetroleum.org/" target="_blank">PA Petroleum Association</a> - Custom Wordpress.</li>
-        </ul>
+        </ul> */}
 
 
         <hr></hr>
         <h3>PERSONAL PROJECTS</h3>
-        <ul>
-          <li><a href="https://chipinfore.com" target="_blank">Chip In Fore</a> - Custom Wordpress. E-Commerce.</li>
-          <li><a href="http://timewellwaste.online" target="_blank">TimeWellWasted.Online</a> - Headless Wordpress. ReactJs. </li>
-          <li><a href="https://housebnb-finished.herokuapp.com/" target="_blank">HouseBnB</a> - ReactJs. Redux. NodeJs. Express.</li>
-        </ul>
+        <a href="http://timewellwaste.online" target="_blank">TimeWellWasted.Online</a> - Headless Wordpress. ReactJs.
+        <a href="http://responsiblyirresponsible.com" target="_blank">ResponsiblyIrresponsible.com</a> - ReactJS. NodeJS.
       </div>
     )
   }
