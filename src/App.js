545bd19c61cb8ad178fadcb3256f7bfee4e584ab
scdozer <scottccardoza@gmail.com>
2021-03-29 17:40:12 -0600

    2020 i work at ib

--- src/App.js
+++ src/App.js
@@ -3,24 +3,10 @@ import router from './router';
 
 class App extends Component {
   render() {
-
-
-    let colors = [
-      'bg-gray-800',
-      'bg-orange-800',
-      'bg-green-900',
-      'bg-teal-900',
-      'bg-blue-900'
-    ]
-    let color = colors[Math.floor(Math.random()*colors.length)];
-    return (
-      <div className={"App " + color}>
-          {router}
-      </div>
-    );
+    let colors = ['bg-gray-800', 'bg-green-900', 'bg-teal-900', 'bg-blue-900'];
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    return <div className={'App h-screen ' + color}>{router}</div>;
   }
 }
 
 export default App;
-
-
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,31 +5,63 @@ export default class Main extends Component {
   render() {
     return (
       <div className="container mx-auto p-10 lg:p-40">
-        <h1 className="text-6xl text-white font-bold leading-none">SCOTT<br/>CARDOZA</h1>
-        <a className="text-white p-4 pl-1 text-2xl " rel="noopener noreferrer" href="https://github.com/scdozer/" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
-        <a className="text-white p-4 text-2xl " rel="noopener noreferrer" href="https://www.linkedin.com/in/scott-cardoza-b5497974/" target="_blank"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
-        <a className="text-white p-4 text-2xl " rel="noopener noreferrer" href="mailto:scottccardoza@gmail.com" target="_blank"><i className="fa fa-envelope-o" aria-hidden="true"></i></a>
+        <h1 className="text-6xl text-white font-bold leading-none">
+          scott cardoza
+        </h1>
+        <a
+          className="text-white p-4 text-2xl "
+          rel="noopener noreferrer"
+          href="mailto:scottccardoza@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-envelope-o" aria-hidden="true"></i>
+        </a>
 
-        <p className="text-5xl text-gray-100 pt-20 font-bold">I make things for the web.</p>
+        <p className="text-4xl text-gray-100 pt-10 font-bold">
+          software developer
+        </p>
 
-        <p className="text-4xl text-gray-100 pt-20">Since Febuary 2015, I have been working at <a className="font-bold text-black" rel="noopener noreferrer" href="https://consumerfocusmarketing.com/" target="_blank">Consumer Focus Marketing</a> as a Web Developer. We do a lot with Wordpress.</p>
+        <p className="text-3xl text-gray-100 pt-0">
+          currently coding alongside an all time crew at{' '}
+          <a
+            className="font-bold text-black"
+            rel="noopener noreferrer"
+            href="https://impossible-bureau.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Impossible Bureau
+          </a>
+          .
+        </p>
 
-        <hr className="mt-20"/>
+        <h2 className="text-3xl text-white pt-20 font-bold">tings:</h2>
 
-        <h2 className="text-6xl text-white pt-20 font-bold">Recent Brags:</h2>
+        <p className=" text-gray-100 ">
+          <a className="font-bold text-black" href="https://github.com/scdozer">
+            github
+          </a>
+        </p>
 
-        <p className="text-4xl text-gray-100 pt-20">I watch a lot of short videos and the ones I enjoyed can be found <a className="font-bold text-black" href="https://timewellwasted.online">here</a>. I made that site with Wordpress and React.</p>
+        <p className=" text-gray-100 ">
+          <a
+            className="font-bold text-black"
+            href="mailto:scottccardoza@gmail.com"
+          >
+            email
+          </a>
+        </p>
 
-        <p className="text-4xl text-gray-100 pt-20">I helped a friend <a className="font-bold text-black" href="https://droneopsco.com/" target="_blank" rel="noopener noreferrer">make this website</a> using Wordpress as a headless CMS using Gatsby and TailWindCSS.</p>
-
-        <p className="text-4xl text-gray-100 pt-20">I have been playing with generative art in P5.js. You can see some of my doodles, <a className="font-bold text-black" href="https://localhost1000.glitch.me">here</a>. It's a resource hog.</p>
-
-        <p className="text-4xl text-gray-100 pt-20">Alongside my friend, Brady, we made a tangible book alongside an app to play AR video with matching images, using Unity and Viewforia. <span className="text-2xl">Pictures/Video soon.</span></p>
-
-        <p className="text-2xl text-gray-100 pt-40"> For more recent projects, check out my <a className="font-bold text-xl text-black" href="https://glitch.com/@scdozer">Glitch</a> or <a className="font-bold text-xl text-black" href="https://github.com/scdozer">GitHub</a>.</p>
-        <p className="text-2xl text-gray-100"> If you want to work on a project, <a href="mailto:scottccardoza@gmail.com">email me</a>.</p>
-
+        <p className=" text-gray-100 ">
+          <a
+            className="font-bold text-black"
+            href="https://timewellwasted.online"
+          >
+            tww
+          </a>
+        </p>
       </div>
-    )
+    );
   }
 }
